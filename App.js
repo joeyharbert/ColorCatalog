0d01522f7baf6c8d6d0b6ec81752f4cbaaa205d5
joeyharbert <joey.harbert@gmail.com>
2020-11-07 18:50:54 -0600

    init

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Joey is so cool! lolol</Text>
       <StatusBar style="auto" />
     </View>
   );

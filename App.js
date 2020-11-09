ee5f5785ab456035d629a605dc94c9a096479b1f
joeyharbert <joey.harbert@gmail.com>
2020-11-08 20:02:56 -0600

    adds shortid and creates useColors custom hook

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList, Alert } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
+import { generate } from 'shortid';
 import ColorButton from './components/ColorButton';
-import defaultColors from './data/defaultColors.json';
 import ColorForm from './components/ColorForm';
+
+const useColors = () => {
+  const [colors, setColors] = useState([]);
+  const addColor = color => {
+    const newColor = {id: generate(), color}
+    setColors([newColor, ...colors ])
+  };
+
+  return {colors, addColor};
+}
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
+  const {colors, addColor} = useColors();
+  
   return (
     <>
-      <ColorForm onNewColor={newColor => Alert.alert(`TODO: add color ${newColor}`)} />
+      <ColorForm onNewColor={addColor} />
       <FlatList 
       style={[styles.container, {backgroundColor}]} 
-      data={defaultColors}
+      data={colors}
       renderItem={({item}) => {
         return(
           <ColorButton key={item.id} onPress={setBackgroundColor} backgroundColor={item.color} />

4f4231e5768c7cbbe8a97a31d35005b53f202c37
joeyharbert <joey.harbert@gmail.com>
2020-11-08 19:52:44 -0600

    add ColorForm

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
-import ColorButton from './components/ColorButton'
-import defaultColors from './data/defaultColors.json'
+import { StyleSheet, View, FlatList, Alert } from 'react-native';
+import ColorButton from './components/ColorButton';
+import defaultColors from './data/defaultColors.json';
+import ColorForm from './components/ColorForm';
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
-    <FlatList 
-    style={[styles.container, {backgroundColor}]} 
-    data={defaultColors}
-    renderItem={({item}) => {
-      return(
-        <ColorButton key={item.id} onPress={setBackgroundColor} backgroundColor={item.color} />
-      );
-    }}/>
+    <>
+      <ColorForm onNewColor={newColor => Alert.alert(`TODO: add color ${newColor}`)} />
+      <FlatList 
+      style={[styles.container, {backgroundColor}]} 
+      data={defaultColors}
+      renderItem={({item}) => {
+        return(
+          <ColorButton key={item.id} onPress={setBackgroundColor} backgroundColor={item.color} />
+        );
+      }}/>
+    </>
   );
 }
 
